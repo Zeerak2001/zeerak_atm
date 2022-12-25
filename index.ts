c92783907809f3e971042e5d4cb24b06e2635218
Zeerak2001 <90219909+Zeerak2001@users.noreply.github.com>
2022-12-25 15:43:35 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,72 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+interface ansType {
+    userID: string,
+    userPin: number,
+    accountType: string,
+    transactionType: string,
+    amount: number
+}
+
+const answer = await inquirer.prompt([
+    {
+        type: "input",
+        name: "userID",
+        message: "Kindly enter you ID: "
+    },
+    {
+        type: "number",
+        name: "userPin",
+        message: "Enter your Pin: "
+    },
+    {
+        type: "list",
+        name: "accountType",
+        choices: ["Saving", "Current"],
+        message: "Select your account type",
+    },
+    {
+        type: "list",
+        name: "transactionType",
+        choices: ["Fast Cash", "Withdraw"],
+        message: "Select your transaction",
+        when(answer){
+            return answer.accountType
+        },
+
+    },
+    {
+        type: "list",
+        name: "amount",
+        choices: ["1000", "2000", "4000", "5000"],
+        message: "Select your amount",
+        when(answer){
+            return answer.transactionType == "Fast Cash"
+        },
+    },
+    {
+        type: "number",
+        name: "amount",
+        message: "Enter your amount",
+        when(answer){
+            return answer.transactionType == "Withdraw"
+        },
+    },
+    
+])
+
+if(answer.userID && answer.userPin){
+    const bal = Math.floor(Math.random()*1000000);
+    console.log(bal);
+    const enterAmount = answer.amount;
+
+    if (bal >= enterAmount){
+        const remaining = bal - enterAmount;
+        console.log("Your remaining balance is ", remaining)
+    }else{
+        console.log("Insufficient Balance")
+    }
+}
+
